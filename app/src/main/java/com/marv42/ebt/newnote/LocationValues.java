ba6505c3054cf18c9c960fef6f5770cead795f9d
Marvin <marv42+github@gmail.com>
2018-07-09 22:17:08 +0200

    make class package private

--- app/src/main/java/com/marv42/ebt/newnote/LocationValues.java
+++ app/src/main/java/com/marv42/ebt/newnote/LocationValues.java
@@ -13,92 +13,49 @@ package com.marv42.ebt.newnote;
 
 import android.content.Context;
 
-
-
-public class LocationValues
-{
+class LocationValues {
    private final String mCountry;
    private final String mCity;
    private final String mPostalCode;
    
    private final boolean mOverwrite;
-   
-   
-   
-   public LocationValues()
-   {
+
+   LocationValues() {
       this("", "", "", false, null);
    }
-   
-   
 
-   public LocationValues(final String  country,
-                         final String  city,
-                         final String  postalCode)
-   {
+   LocationValues(final String country, final String city, final String postalCode) {
       this(country, city, postalCode, false, null);
    }
-   
-   
-   
-   public LocationValues(final String  country,
-                         final String  city,
-                         final String  postalCode,
-                         final boolean overwrite)
-   {
+
+   LocationValues(final String country, final String city, final String postalCode, final boolean overwrite) {
       this(country, city, postalCode, overwrite, null);
    }
-   
-   
-   
-   public LocationValues(final String  country,
-                         final String  city,
-                         final String  postalCode,
-                         final boolean overwrite,
-                         final Context context)
-   {
+
+   LocationValues(final String country, final String city, final String postalCode,
+                  final boolean overwrite, final Context context) {
       mCountry        = country;
       mCity           = city;
       mPostalCode     = postalCode;
       mOverwrite      = overwrite;
-      
       if (context != null)
          ((ThisApp) context.getApplicationContext())
-            .setLocationValues(new LocationValues(country,
-                                                  city,
-                                                  postalCode,
-                                                  overwrite));
+            .setLocationValues(new LocationValues(country, city, postalCode, overwrite));
    }
-   
-   
-   
-   public String
-   getCountry()
-   {
+
+   String getCountry() {
       return mCountry;
    }
    
-   
-   
-   public String
-   getCity()
-   {
+   String getCity() {
       return mCity;
    }
    
-   
-   
-   public String
-   getPostalCode()
-   {
+   String getPostalCode() {
       return mPostalCode;
    }
-   
-   
-   
-   public boolean
-   canOverwrite()
-   {
+
+   boolean canOverwrite() {
       return mOverwrite;
    }
 }
