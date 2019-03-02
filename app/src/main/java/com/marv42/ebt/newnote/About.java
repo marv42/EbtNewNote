4580e61bc17af0667fb0db64c9c02ff821a8a010
Marvin <marv42+github@gmail.com>
2019-03-03 00:48:56 +0100

    take drawable from resource

--- app/src/main/java/com/marv42/ebt/newnote/About.java
+++ app/src/main/java/com/marv42/ebt/newnote/About.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * 
+ *
  * Contributors:
  *     marvin - initial API and implementation
  ******************************************************************************/
@@ -14,39 +14,36 @@ package com.marv42.ebt.newnote;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.MenuItem.OnMenuItemClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import static com.marv42.ebt.newnote.EbtNewNote.LOG_TAG;
-
 public class About implements OnMenuItemClickListener {
-   private final Context mContext;
-   
-   About(Context context) {
-      mContext = context;
-   }
-   
-   public boolean onMenuItemClick(MenuItem item) {
-      final Dialog dialog = new Dialog(mContext);
-      dialog.setContentView(R.layout.about);
-      dialog.setTitle(R.string.app_name);
-      
-      String versionNumber = "x.y.z";
-      try {
-         versionNumber = mContext.getPackageManager().getPackageInfo(
-               mContext.getPackageName(), 0).versionName;
-      } catch (NameNotFoundException e) {
-         Log.e(LOG_TAG, "Package info not found", e);
-      }
+    private final Context mContext;
 
-      ((TextView)dialog.findViewById(R.id.about_version)).setText(String.format(mContext.getString(R.string.version), versionNumber));
-      ((ImageView)dialog.findViewById(R.id.about_image)).setImageResource(R.drawable.ic_ebt);
-      ((TextView)dialog.findViewById(R.id.about_text)).setText(
-              String.format("%1$s %2$s", mContext.getString(R.string.app_name), mContext.getString(R.string.about_text)));
-      dialog.show();
-      return true;
-   }
+    About(Context context) {
+        mContext = context;
+    }
+
+    public boolean onMenuItemClick(MenuItem item) {
+        final Dialog dialog = new Dialog(mContext);
+        dialog.setContentView(R.layout.about);
+        dialog.setTitle(R.string.app_name);
+
+        String versionNumber = "x.y.z";
+        try {
+            versionNumber = mContext.getPackageManager().getPackageInfo(
+                    mContext.getPackageName(), 0).versionName;
+        } catch (NameNotFoundException ignored) {
+        }
+
+        ((TextView) dialog.findViewById(R.id.about_version)).setText(
+                String.format(mContext.getString(R.string.version), versionNumber));
+        ((TextView) dialog.findViewById(R.id.about_text)).setText(
+                String.format("%1$s %2$s", mContext.getString(R.string.app_name),
+                        mContext.getString(R.string.about_text)));
+        dialog.show();
+        return true;
+    }
 }
