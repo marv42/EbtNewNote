1f44afee8386aaded7873d44b0973c23482fa829
Marvin <marv42+github@gmail.com>
2019-03-18 22:09:44 +0100

    dagger-android is implicit

--- app/src/main/java/com/marv42/ebt/newnote/di/ApplicationComponent.java
+++ app/src/main/java/com/marv42/ebt/newnote/di/ApplicationComponent.java
@@ -5,12 +5,12 @@ import com.marv42.ebt.newnote.ThisApp;
 import javax.inject.Singleton;
 
 import dagger.Component;
-import dagger.android.AndroidInjectionModule;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
 @Singleton
-@Component(modules = {AndroidInjectionModule.class, AndroidSupportInjectionModule.class, ApplicationModule.class})
+@Component(modules = {/*AndroidInjectionModule.class,*/ AndroidSupportInjectionModule.class,
+        ApplicationModule.class})
 public interface ApplicationComponent extends AndroidInjector<ThisApp> {
     @Component.Builder
     abstract class Builder extends AndroidInjector.Builder<ThisApp> {
