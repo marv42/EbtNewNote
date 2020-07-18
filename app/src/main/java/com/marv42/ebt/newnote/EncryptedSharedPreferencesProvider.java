bd954b73fb3310b2462de5126a41e219993d21ed
Marvin <marv42+github@gmail.com>
2020-07-19 00:02:28 +0200

    adapt to new EncryptedSharedPreferences

--- app/src/main/java/com/marv42/ebt/newnote/EncryptedSharedPreferencesProvider.java
+++ app/src/main/java/com/marv42/ebt/newnote/EncryptedSharedPreferencesProvider.java
@@ -1,17 +1,28 @@
 package com.marv42.ebt.newnote;
 
 import android.content.SharedPreferences;
+import android.security.keystore.KeyGenParameterSpec;
+import android.util.Log;
 
 import androidx.security.crypto.EncryptedSharedPreferences;
-import androidx.security.crypto.MasterKeys;
+import androidx.security.crypto.MasterKey;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 import javax.inject.Inject;
 
+import static android.security.keystore.KeyProperties.BLOCK_MODE_GCM;
+import static android.security.keystore.KeyProperties.ENCRYPTION_PADDING_NONE;
+import static android.security.keystore.KeyProperties.PURPOSE_DECRYPT;
+import static android.security.keystore.KeyProperties.PURPOSE_ENCRYPT;
+import static androidx.security.crypto.EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV;
+import static androidx.security.crypto.EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM;
+import static androidx.security.crypto.MasterKey.DEFAULT_MASTER_KEY_ALIAS;
+
 public class EncryptedSharedPreferencesProvider {
     private static final String ENCRYPTED_SHARED_PREFERENCES_FILE_NAME = "encrypted_shared_prefs";
+    private static final int KEY_SIZE = 256;
 
     private ThisApp mApp;
 
@@ -20,12 +31,21 @@ public class EncryptedSharedPreferencesProvider {
         mApp = app;
     }
 
+    private MasterKey getMasterKey() throws GeneralSecurityException, IOException {
+        KeyGenParameterSpec spec = new KeyGenParameterSpec.Builder(
+                DEFAULT_MASTER_KEY_ALIAS, PURPOSE_ENCRYPT | PURPOSE_DECRYPT)
+                .setBlockModes(BLOCK_MODE_GCM)
+                .setEncryptionPaddings(ENCRYPTION_PADDING_NONE)
+                .setKeySize(KEY_SIZE)
+                .build();
+        return new MasterKey.Builder(mApp)
+//                    .setKeyScheme(AES256_GCM)
+                .setKeyGenParameterSpec(spec)
+                .build();
+    }
+
     SharedPreferences getEncryptedSharedPreferences() throws GeneralSecurityException, IOException {
-        String masterKeyAlias;
-        masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
-        return EncryptedSharedPreferences.create(ENCRYPTED_SHARED_PREFERENCES_FILE_NAME,
-                masterKeyAlias, mApp,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
+        return EncryptedSharedPreferences.create(mApp, ENCRYPTED_SHARED_PREFERENCES_FILE_NAME,
+                getMasterKey(), AES256_SIV, AES256_GCM);
     }
 }
