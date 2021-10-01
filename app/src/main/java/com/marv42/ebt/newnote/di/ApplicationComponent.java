96508cdd5e0bfb8711f33f3f48674218ee13eb95
Marvin <marv42+github@gmail.com>
2021-10-01 21:26:38 +0200

    put Modules and Components into the same files

--- app/src/main/java/com/marv42/ebt/newnote/di/ApplicationComponent.java
+++ app/src/main/java/com/marv42/ebt/newnote/di/ApplicationComponent.java
@@ -8,13 +8,78 @@
 
 package com.marv42.ebt.newnote.di;
 
+import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
+
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+
+import com.marv42.ebt.newnote.ApiCaller;
+import com.marv42.ebt.newnote.EbtNewNote;
 import com.marv42.ebt.newnote.ThisApp;
+import com.marv42.ebt.newnote.preferences.EncryptedPreferenceDataStore;
+import com.marv42.ebt.newnote.preferences.SettingsActivity;
+import com.marv42.ebt.newnote.preferences.SharedPreferencesHandler;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
 import dagger.android.AndroidInjector;
+import dagger.android.ContributesAndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
+
+@Module(subcomponents = {EbtNewNoteComponent.class, // FetchAddressIntentServiceComponent.class,
+        SettingsComponent.class})
+abstract class ApplicationModule {
+
+//    @Singleton
+//    abstract Context bindContext(ThisApp app);
+    // prefer static over virtual: https://developer.android.com/training/articles/perf-tips.html#PreferStatic
+//    @Provides
+//    @Singleton
+//    static Context provideContext(@NonNull ThisApp app) {
+//        return app.getApplicationContext();
+//    }
+
+    @Provides
+    @Singleton
+    static SharedPreferences provideSharedPreferences(@NonNull ThisApp app) {
+        return getDefaultSharedPreferences(app);
+    }
+
+    @Provides
+    @Singleton
+    static SharedPreferencesHandler provideSharedPreferencesHandler(
+            @NonNull ThisApp app, @NonNull SharedPreferences sharedPreferences) {
+        return new SharedPreferencesHandler(app, sharedPreferences);
+    }
+
+    @Provides
+    @Singleton
+    static EncryptedPreferenceDataStore provideEncryptedPreferenceDataStore(@NonNull ThisApp app) {
+        return new EncryptedPreferenceDataStore(app);
+    }
+
+    @Provides
+    @Singleton
+    static ApiCaller provideApiCaller(EncryptedPreferenceDataStore dataStore) {
+        return new ApiCaller(dataStore);
+    }
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = EbtNewNoteModule.class)
+    abstract EbtNewNote contributeEbtNewNoteInjector();
+
+//    @IntentServiceScope
+//    @ContributesAndroidInjector(modules = FetchAddressIntentServiceModule.class)
+//    abstract FetchAddressIntentService contributeFetchAddressIntentServiceInjector();
+
+    @SettingsScope
+    @ContributesAndroidInjector(modules = SettingsModule.class)
+    abstract SettingsActivity contributeSettingsActivityInjector();
+}
 
 @Singleton
 @Component(modules = {AndroidSupportInjectionModule.class, ApplicationModule.class})
--- app/src/main/java/com/marv42/ebt/newnote/di/SettingsComponent.java
+++ app/src/main/java/com/marv42/ebt/newnote/di/SettingsComponent.java
@@ -10,8 +10,18 @@ package com.marv42.ebt.newnote.di;
 
 import com.marv42.ebt.newnote.preferences.SettingsActivity;
 
+import dagger.Module;
 import dagger.Subcomponent;
 import dagger.android.AndroidInjector;
+import dagger.android.ContributesAndroidInjector;
+
+@Module(subcomponents = {SettingsFragmentComponent.class})
+abstract class SettingsModule {
+
+    @SettingsFragmentScope
+    @ContributesAndroidInjector(modules = SettingsFragmentModule.class)
+    abstract SettingsActivity.SettingsFragment contributeSettingsFragmentInjector();
+}
 
 @SettingsScope
 @Subcomponent(modules = {SettingsModule.class})
