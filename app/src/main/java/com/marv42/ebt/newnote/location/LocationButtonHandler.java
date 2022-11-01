5b5d6bb60a9b7bc7742497f84e3901f276b6e0b5
Marvin <marv42+github@gmail.com>
2022-11-01 18:40:05 +0100

    rework location button actions
    This fixes https://github.com/marv42/EbtNewNote/issues/14

--- app/src/main/java/com/marv42/ebt/newnote/location/LocationButtonHandler.java
+++ app/src/main/java/com/marv42/ebt/newnote/location/LocationButtonHandler.java
@@ -23,9 +23,12 @@ import javax.inject.Inject;
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.widget.Toast.LENGTH_LONG;
+import static android.widget.Toast.LENGTH_SHORT;
 import static androidx.core.content.PermissionChecker.PERMISSION_GRANTED;
 import static androidx.core.content.PermissionChecker.checkSelfPermission;
 import static com.marv42.ebt.newnote.EbtNewNote.LOCATION_PERMISSION_REQUEST_CODE;
+
+import java.util.Objects;
 
 public class LocationButtonHandler {
 
@@ -41,34 +44,31 @@ public class LocationButtonHandler {
     public void clicked() {
         if (activity == null)
             throw new IllegalStateException("No activity");
-        if (!locationPermissionsGranted())
+        if (!isLocationEnabled()) {
+            Toast.makeText(activity, R.string.location_not_enabled, LENGTH_SHORT).show();
+            Toast.makeText(activity, R.string.location_please_enable, LENGTH_LONG).show();
+            app.startLocationProviderChangedReceiver();
             return;
-        if (!locationIsEnabled())
+        }
+        final int coarseLocationPermission = checkSelfPermission(app, ACCESS_COARSE_LOCATION);
+        if (coarseLocationPermission != PERMISSION_GRANTED) {
+            requestLocationPermissions();
             return;
-        if (checkSelfPermission(app, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED)
+        }
+        final int fineLocationPermission = checkSelfPermission(app, ACCESS_FINE_LOCATION);
+        if (fineLocationPermission != PERMISSION_GRANTED)
             Toast.makeText(activity, R.string.location_no_gps, LENGTH_LONG).show();
         app.startLocationTask();
     }
 
-    private boolean locationPermissionsGranted() {
-        if (activity != null &&
-                checkSelfPermission(app, ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED &&
-                checkSelfPermission(app, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(activity,
-                    new String[]{ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
-            return false;
-        }
-        return true;
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager == null || locationManager.isLocationEnabled();
     }
 
-    private boolean locationIsEnabled() {
-        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager != null && !locationManager.isLocationEnabled()) {
-            Toast.makeText(activity, R.string.location_not_enabled, LENGTH_LONG).show();
-            app.startLocationProviderChangedReceiver();
-            return false;
-        }
-        return true;
+    private void requestLocationPermissions() {
+        ActivityCompat.requestPermissions(Objects.requireNonNull(activity),
+                new String[]{ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION},
+                LOCATION_PERMISSION_REQUEST_CODE);
     }
 }
